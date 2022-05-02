4328a71fc46bd2668daad7208414b82984e0ce8b
Charlie <84060702+cjayversace@users.noreply.github.com>
2022-05-02 15:40:33 -0400

    Update PythonCC.py

--- PythonCC.py
+++ PythonCC.py
@@ -1,14 +1,17 @@
-import sys
+import sys #python library
 #creation of the ceaser cypher
 
 letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 #length = 26 letters of alph
+
 shift = sys.argv
 #shift = int(input("Enter the number of character shifts wanted: "))
+
 message = input("Input message: ")
 message= message.upper()
 empty_string = ""
 minimum_shift = 27 - shift
+
 for i in sys.stdin:
     if i == " ":
         empty_string += i
@@ -21,6 +24,5 @@ for i in sys.stdin:
             elif position >= minimum_shift:
                 position = positon + shift - 26
                 empty_string += letters[position]
-
 length = len(empty_string)
 counter = 0
